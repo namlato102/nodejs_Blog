092a0b4d726cb737061a6a9365f44a8151802ca0
ddang001 <dinhnam.dang@student.hs-rm.de>
2023-07-03 12:17:07 +0200

    new commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
+const handlebars = require('express_handlebars') //template engine
 const app = express()
 const port = 3000
 
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+
+app.engine('handlebars', handlebars()); //dinh nghia
+app.set('view engine', 'handlebars'); // xet view engine = handlebars
 
 app.get('/', (req, res) => { // get('/trang chu') dinh nghia tuyen duong
-  res.send(
-    <h1>Hello World!</h1>
-  );
+    res.send("<h1>Hello World!</h1>");
 })
 
 //127.0.0.1 localhost

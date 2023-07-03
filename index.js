9957bb42e1ac2ba012d1fba4fb3a5dfc021c8090
ddang001 <dinhnam.dang@student.hs-rm.de>
2023-07-03 11:55:44 +0200

    js file

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
+
 app.get('/trang-chu', (req, res) => { // get('/trang chu') dinh nghia tuyen duong
   res.send('Hello World!')
 })

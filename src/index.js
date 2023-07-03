9c4c4b436d6e2d5ddd4d3a648a4eeb5b94c54e66
ddang001 <dinhnam.dang@student.hs-rm.de>
2023-07-03 14:41:32 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars') ;
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
 
 //HTTP logger
 app.use(morgan('combined'));

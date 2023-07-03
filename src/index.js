9632b08905ca387d736224c84367a2af6487cde9
ddang001 <dinhnam.dang@student.hs-rm.de>
2023-07-03 18:19:23 +0200

    added

--- src/index.js
+++ src/index.js
@@ -5,17 +5,26 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+//static file img/digital-clock.png
+app.use(express.static(path.join(__dirname, "public")));
+
 //HTTP logger
 app.use(morgan('combined'));
 
 //Template Engine
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars'); // xet view engine = handlebars
+app.engine('hbs', handlebars.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs'); // xet view engine = handlebars
 app.set('views', path.join(__dirname, 'resources/views'));
 app.use(express.static('views'));
 
 app.get('/', (req, res) => { // get('/trang chu') dinh nghia tuyen duong
     res.render('home');
+})
+
+app.get('/news', (req, res) => { // get('/trang chu') dinh nghia tuyen duong
+  res.render('news');
 })
 
 //127.0.0.1 localhost
